cbdfe90ad9153542a499ea6b1e667d2ec44f459d
David Foote <david.a.foote@gmail.com>
2019-06-18 01:30:37 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-  word[.match/w\aeiou/])
+   return word.match?(/^[aeiou]/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+ return text.scan(/un[\w]+ing/)
 end
 
 def words_five_letters_long(text)
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  
 end
 
 def valid_phone_number?(phone)
