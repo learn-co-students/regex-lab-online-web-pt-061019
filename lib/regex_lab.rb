b80489ab53cf8d96dd9df61e44d39c48f1282354
Huy Do <huyddo40@gmail.com>
2019-06-22 23:43:41 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,14 @@
-def starts_with_a_vowel?(word)
+def starts_with_a_vowel?(array)
+  array.select {|w| w.(/^[aeiou]/)}
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/^un$ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.grep(/^\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -15,5 +16,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/(\d+)-(\d+)-(\d+)/)
 end
