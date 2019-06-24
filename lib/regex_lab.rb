84a87d1cd3b9a98c2f2a05c7d726d17a2c0aa98c
David Foote <david.a.foote@gmail.com>
2019-06-24 23:47:22 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,13 +7,13 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  return text.scan(/\w{5}[A-Za-z]/)
+  return text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  
+  return text.match?(/\b[A-Z]/)
 end
 
 def valid_phone_number?(phone)
-
+  return phone.match?(/(?:\d{1}\s)?\(?(\d{3})\)?-?\s?(\d{3})-?\s?(\d{4})/)
 end
