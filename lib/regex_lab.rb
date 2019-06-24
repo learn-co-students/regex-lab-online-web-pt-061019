4d3a2eb37c9aa7ae0c99f44e227fa4cd10737822
Huy Do <huyddo40@gmail.com>
2019-06-24 20:18:05 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,17 +5,18 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/un\w+ing/)
-  binding.pry
+
 end
 
 def words_five_letters_long(text)
-  text.grep(/^\w{5}/)
+  text.scan(/\b[a-zA-Z]{5}\b/)
+  # binding.pry
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match?(/^[A-Z].*\W$/)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/(\d+)-(\d+)-(\d+)/)
+  phone.match(/\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/)
 end
